76592aba34ea9ba2bb80854238f294fdcd5e3945
pagdan <pagdan@proton.me>
2025-10-27 17:41:43 +0800

    feat: Wellbeing page

---
+++ app/(dashboard)/wellbeing/emotional/page.tsx
@@ -0,0 +1,4 @@
+// app/student-life/bus-routes/page.tsx
+export default function BusRoutesPage() {
+  return <div>Bus Routes Page - Coming Soon!</div>;
+}
---
+++ app/(dashboard)/wellbeing/social/page.tsx
@@ -0,0 +1,4 @@
+// app/wellbeing/social/bus-routes/page.tsx
+export default function SocialPage() {
+  return <div>Social Page - Coming Soon!</div>;
+}
